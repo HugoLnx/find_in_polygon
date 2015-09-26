d90e77a43f5756eb9ff00fae61b09c41c311bfc4
Hugo Roque <hugolnx@gmail.com>
2015-09-26 03:59:13 -0300

    isn't compiling, but search is working

--- Main.scala
+++ Main.scala
@@ -1,11 +1,40 @@
 import com.hugolnx._
+import java.io._
 
 object Main {
+	// longMin: -73.9904499689999
+	// longMax: -22.256396161353557
+	// latMin: -41.29240741890326
+	// latMax: 13.864926899496986
 	def main(args: Array[String]) {
-		val index = CellsIndex
+		// val affiliates = ShapefileReader.readShapefile("./channel_map_2015_v6/channel_map_2015_v6.shp")
+		// val finder = new Finder[Affiliate]()
+		// var i = 0
+		// for(longInt <- -73991 until -22258) {
+		// 	for(latInt <- -41293 until 13865) {
+		// 		var pt = (longInt/1000.0, latInt/1000.0)
+		// 		var optAffiliate = finder.findOn(pt, affiliates)
+		// 		var id = optAffiliate.map{affiliate=>affiliate.id}.getOrElse(null)
+		// 		val oos = new ObjectOutputStream(new FileOutputStream("./res/test/fixtures/affiliates/pos" + longInt + "x" + latInt + ".txt"))
+		// 		oos.writeObject(id)
+		// 		oos.close
+		// 		if(i % 100000 == 0) {
+		// 			println(i)
+		// 		}
+		// 		i += 1
+		// 	}
+		// }
 	}
 
-	def getIndex() : CellsIndex = {
+	def getAffiliate() : Option[Affiliate] = {
+		val index = getIndex
+		val finder = new Finder[Affiliate]()
+		val optFinder = new OptimalFinder[Affiliate](index, finder)
+		val pt = (-46.60, -23.30)
+		optFinder.find(pt._1, pt._2)
+	}
+
+	def getIndex() : CellsIndex[Affiliate] = {
 		val affiliates = ShapefileReader.readShapefile("./channel_map_2015_v6/channel_map_2015_v6.shp")
 
 		CellsIndex.to(affiliates)
--- src/main/scala/com/hugolnx/domain.scala
+++ src/main/scala/com/hugolnx/domain.scala
@@ -1,121 +1,229 @@
-package com.hugolnx
+package com
 
 import geotrellis.shapefile
 import geotrellis.shapefile.{Parser,Record}
 import com.vividsolutions.jts.geom._
 
-class Affiliate(_id:Int, _area:Geometry) {
-	def id: Int = _id
-	def area: Geometry = _area
-}
+package object hugolnx {
+	type Area[T] = (Geometry, T)
 
-object ShapefileReader {
-	def readShapefile(path:String) : Seq[Affiliate] = {
-		val records:Seq[Record] = shapefile.Parser(path)
-
-		records.map{rec => new Affiliate(rec.id, multipolygonFrom(rec))}
+	class Affiliate(_id:Int) {
+		def id: Int = _id
 	}
 
-	def multipolygonFrom(rec:Record) : MultiPolygon = {
-		val gm = new GeometryFactory()
-		gm.createMultiPolygon(Array(rec.g.asInstanceOf[Polygon]))
-	}
-}
+	object ShapefileReader {
+		def readShapefile(path:String) : Seq[Area[Affiliate]] = {
+			val records:Seq[Record] = shapefile.Parser(path)
 
-class CellsIndex(longSize : Int, latSize : Int, longMin : Double, latMin : Double) {
-	var index : Map[Array[Int],Seq[Affiliate]] = Map()//new Array[Seq[Affiliate]](longSize*latSize)
-
-	def get(long: Double, lat: Double) : Seq[Affiliate] = {
-		index(Array(longGr(long), latGr(lat)))
-		//index(inxFor(longGr(long), latGr(lat)))
-	}
-
-	def longGr(long:Double) = (CellsIndex.coordToInt(long) - CellsIndex.coordToInt(longMin)) / CellsIndex.CELL_SIZE
-	def latGr(lat:Double) = (CellsIndex.coordToInt(lat) - CellsIndex.coordToInt(latMin)) / CellsIndex.CELL_SIZE
-
-	def set(longGr : Int, latGr : Int, affiliates : Seq[Affiliate]) {
-		//index(inxFor(longGr,latGr)) = affiliates
-		index += (Array(longGr,latGr) -> affiliates)
-	}
-
-	def inxFor(longGr : Int, latGr : Int) : Int = longGr + latGr*longSize
-}
-
-object CellsIndex {
-	val CELL_SIZE = 1000
-
-	def to(all_affiliates : Seq[Affiliate]) : CellsIndex = {
-		var affiliates = all_affiliates
-		var longMin = 190.0
-		var longMax = -190.0
-		var latMin = 190.0
-		var latMax = -190.0
-		for(affiliate <- affiliates){
-			val env = affiliate.area.getEnvelopeInternal
-			if(env.getMinX < longMin) longMin = env.getMinX
-			if(env.getMinY < latMin ) latMin  = env.getMinY
-			if(env.getMaxX > longMax) longMax = env.getMaxX
-			if(env.getMaxY > latMax ) latMax  = env.getMaxY
+			records.map{rec => (multipolygonFrom(rec), new Affiliate(rec.id))}
 		}
-		
-		val longSize = coordToInt(longMax) - coordToInt(longMin)
-		val latSize = coordToInt(latMax) - coordToInt(latMin)
 
-		val qntLongCells = if(longSize % CELL_SIZE == 0) longSize / CELL_SIZE else longSize / CELL_SIZE + 1
-		val qntLatCells = if(latSize % CELL_SIZE == 0) latSize / CELL_SIZE else latSize / CELL_SIZE + 1
-		println("longMin: " + longMin)
-		println("longMax: " + longMax)
-		println("latMin: " + latMin)
-		println("latMax: " + latMax)
-		println("longSize: " + longSize)
-		println("latSize: " + latSize)
-		println("longCells: " + qntLongCells)
-		println("latCells: " + qntLatCells)
-		val index = new CellsIndex(qntLongCells, qntLatCells, longMin, latMin)
+		def multipolygonFrom(rec:Record) : MultiPolygon = {
+			val gm = new GeometryFactory()
+			gm.createMultiPolygon(Array(rec.g.asInstanceOf[Polygon]))
+		}
+	}
 
-		for(longGr <- 0 until qntLongCells) {
-			for(latGr <- 0 until qntLatCells) {
-				val localLongMin = (longMin      + ( longGr * CELL_SIZE )  / 1000.0)
-				val localLongMax = (localLongMin + ( (CELL_SIZE-1)      )  / 1000.0)
-				val localLatMin =  (latMin       + ( latGr * CELL_SIZE  )  / 1000.0)
-				val localLatMax =  (localLatMin  + ( (CELL_SIZE-1)      )  / 1000.0)
-
-				var cell = newPolygon(
-					Array(localLongMin, localLatMax),
-					Array(localLongMin, localLatMin),
-					Array(localLongMax, localLatMin),
-					Array(localLongMax, localLatMax)
-				)
-
-				val selectedAffiliates = affiliates.filter{affiliate =>
-					try {
-						cell.crosses(affiliate.area)
-					} catch {
-						case ex: TopologyException => {
-							//println("Crosses exception in Affiliate " + affiliate.id + " and cell " + cell)
-							true
-						}
+	class Finder[T]() {
+		def findOn(point:(Double,Double), areas:Seq[Area[T]]) : Option[T] = {
+			val gm = new GeometryFactory()
+			val pt = gm.createPoint(new Coordinate(point._1,point._2))
+			areas.find{area =>
+				try {
+					pt.coveredBy(area._1)
+				} catch {
+					case e: TopologyException => {
+						//println("" + point._1 + "," + point._2)
+						true
 					}
-		    }
-				index.set(longGr, latGr, selectedAffiliates)
+					case _ => false
+				}
+			}.map{area => area._2}
+		}
+	}
+
+	class CellsGrid(longMin:Double, latMin:Double, longMax:Double, latMax:Double, cellSize:Int, precision:Double) {
+		def longSize = coordToInt(longMax) - coordToInt(longMin)
+		def latSize = coordToInt(latMax) - coordToInt(latMin)
+
+		def qntLongCells = {
+			if(longSize % cellSize == 0) {
+				longSize / cellSize
+			} else {
+				longSize / cellSize + 1
 			}
 		}
-		
-		return index
+
+		def qntLatCells = {
+			if(latSize % cellSize == 0) {
+				latSize / cellSize
+			} else {
+				latSize / cellSize + 1
+			}
+		}
+
+		def longCell(long:Double) = {
+			val longInt = coordToInt(long)
+			val longMinInt = coordToInt(longMin)
+			(longInt - longMinInt) / cellSize
+		}
+
+		def latCell(lat:Double) = {
+			val latInt = coordToInt(lat)
+			val latMinInt = coordToInt(latMin)
+			(latInt - latMinInt) / cellSize
+		}
+
+		def eachCell(callback: (Int,Int) => _) {
+			for(cellLong <- 0 until qntLongCells) {
+				for(cellLat <- 0 until qntLatCells) {
+					callback(cellLong, cellLat)
+				}
+			}
+		}
+
+		def cellPolygon(cellLong:Int, cellLat:Int) = {
+			val cellLongMin = (longMin      + ( cellLong * cellSize )  / precision)
+			val cellLongMax = (cellLongMin  + ( (cellSize-1)        )  / precision)
+			val cellLatMin  = (latMin       + ( cellLat * cellSize  )  / precision)
+			val cellLatMax  = (cellLatMin   + ( (cellSize-1)        )  / precision)
+
+			newPolygon(
+				Array(cellLongMin, cellLatMax),
+				Array(cellLongMin, cellLatMin),
+				Array(cellLongMax, cellLatMin),
+				Array(cellLongMax, cellLatMax)
+			)
+		}
+
+		def newPolygon(pt1 : Array[Double], pt2 : Array[Double], pt3 : Array[Double], pt4 : Array[Double]) : Polygon = {
+			val gm = new GeometryFactory()
+			gm.createPolygon(gm.createLinearRing(Array(
+				new Coordinate(pt1(0), pt1(1)),
+				new Coordinate(pt2(0), pt2(1)),
+				new Coordinate(pt3(0), pt3(1)),
+				new Coordinate(pt4(0), pt4(1)),
+				new Coordinate(pt1(0), pt1(1))
+			)))
+		}
+
+		def coordToInt(coord: Double) : Int = {
+			(math rint coord * precision).toInt
+		}
 	}
 
-	def newPolygon(pt1 : Array[Double], pt2 : Array[Double], pt3 : Array[Double], pt4 : Array[Double]) : Polygon = {
-		val gm = new GeometryFactory()
-		gm.createPolygon(gm.createLinearRing(Array(
-			new Coordinate(pt1(0), pt1(1)),
-			new Coordinate(pt2(0), pt2(1)),
-			new Coordinate(pt3(0), pt3(1)),
-			new Coordinate(pt4(0), pt4(1)),
-			new Coordinate(pt1(0), pt1(1))
-		)))
+	object CellsGrid {
+		def build[T](areas : Seq[Area[T]], cellSize:Int, precision:Double) : CellsGrid = {
+			var longMin = 190.0
+			var longMax = -190.0
+			var latMin = 190.0
+			var latMax = -190.0
+			for((area,_) <- areas){
+				val env = area.getEnvelopeInternal
+				if(env.getMinX < longMin) longMin = env.getMinX
+				if(env.getMinY < latMin ) latMin  = env.getMinY
+				if(env.getMaxX > longMax) longMax = env.getMaxX
+				if(env.getMaxY > latMax ) latMax  = env.getMaxY
+			}
+
+			println("longMin: " + longMin)
+			println("longMax: " + longMax)
+			println("latMin: " + latMin)
+			println("latMax: " + latMax)
+			var grid = new CellsGrid(longMin, latMin, longMax, latMax, cellSize, precision)
+
+			println("longSize: " + grid.longSize)
+			println("latSize: " + grid.latSize)
+			println("longCells: " + grid.qntLongCells)
+			println("latCells: " + grid.qntLatCells)
+			grid
+		}
 	}
 
-	def coordToInt(coord: Double) : Int = {
-		(math rint coord * 1000).toInt
+	class CellsMap[T](qntLongCells:Int, qntLatCells:Int) {
+		var memory : Array[T] = new Array(qntLongCells*qntLatCells)
+
+		def get(longGr: Int, latGr: Int) : Seq[Area[T]] = {
+			memory(indexFor(longGr, latGr))
+		}
+
+		def set(longGr : Int, latGr : Int, value : Seq[Area[T]]) {
+			memory(indexFor(longGr,latGr)) = value
+		}
+
+		def indexFor(longGr : Int, latGr : Int) : Int = {
+			longGr + latGr*qntLongCells
+		}
+	}
+
+	class PreciseMap(qntLongCells:Int, qntLatCells:Int) {
+		var memory : Array[Boolean] = new Array(qntLongCells*qntLatCells)
+
+		def get(longGr: Int, latGr: Int) : Boolean = {
+			memory(indexFor(longGr, latGr))
+		}
+
+		def set(longGr : Int, latGr : Int, value : Boolean) {
+			memory(indexFor(longGr,latGr)) = value
+		}
+
+		def indexFor(longGr : Int, latGr : Int) : Int = {
+			longGr + latGr*qntLongCells
+		}
+	}
+
+	class OptimalFinder[T](index:CellsIndex[T], finder:Finder[T]) {
+		def find(long:Double, lat:Double) : Option[T] = {
+			var areas = index.get(long, lat)
+			if(index.wasPreciselyFound(long, lat)) {
+				return Some(areas(0).get()._2)
+			} else {
+				return finder.findOn((long, lat), areas)
+			}
+		}
+	}
+
+	class CellsIndex[T](map:CellsMap[Seq[Area[T]]], preciseMap:PreciseMap, grid:CellsGrid) {
+		def get(long:Double, lat:Double) = {
+			map.get(grid.longCell(long), grid.latCell(lat))
+		}
+
+		def wasPreciselyFound(long:Double, lat:Double) : Boolean = {
+			preciseMap.get(grid.longCell(long), grid.latCell(lat))
+		}
+	}
+
+	object CellsIndex {
+		val CELL_SIZE = 100
+		val PRECISION = 3
+
+		def to[T](areas : Seq[Area[T]]) : CellsIndex[T] = {
+			val grid = CellsGrid.build(areas, CELL_SIZE, math.pow(10, PRECISION))
+			val map = new CellsMap[Seq[Area[T]]](grid.qntLongCells, grid.qntLatCells)
+			val preciseMap = new PreciseMap(grid.qntLongCells, grid.qntLatCells)
+
+			grid.eachCell { (cellLong:Int, cellLat:Int) =>
+				var cellPolygon = grid.cellPolygon(cellLong, cellLat)
+
+				val selectedAreas = areas.filter{ tuple =>
+					cellPolygon.intersects(tuple._1)
+				}
+				map.set(cellLong, cellLat, selectedAreas)
+				var preciselyFound = false
+				if(selectedAreas.size == 1) {
+					var area = selectedAreas(0)
+					try {
+						if(area._1.covers(cellPolygon)) {
+							preciselyFound = true
+						}
+					} catch {
+						case e: TopologyException => println("TopologyException")
+					}
+				}
+				preciseMap.set(cellLong, cellLat, preciselyFound)
+			}
+			
+			return new CellsIndex(map, preciseMap, grid)
+		}
 	}
 }
